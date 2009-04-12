1aba5457637c85039b3f5cc926a7bcf96977c7e2
kspes <kspes@55e3bb1b-93b2-4088-9604-7a9e33d1704e>
2009-04-12 21:22:39 +0000

    * removed old code from player.cpp * migrated multiple_videos demo to new demo code structure
    git-svn-id: https://svn.code.sf.net/p/ogrevideo/code/trunk@72 55e3bb1b-93b2-4088-9604-7a9e33d1704e

--- demos/multiple_videos/multiple_video.cpp
+++ demos/multiple_videos/multiple_video.cpp
@@ -1,178 +1,52 @@
-#include "OgreExternalTextureSourceManager.h"
-#include "ExampleApplication.h"
-#include "TheoraVideoManager.h"
-#include "TheoraVideoClip.h"
+/************************************************************************************
+This source file is part of the TheoraVideoPlugin ExternalTextureSource PlugIn 
+for OGRE3D (Object-oriented Graphics Rendering Engine)
+For latest info, see http://ogrevideo.sourceforge.net/
+*************************************************************************************
+Copyright © 2008-2009 Kresimir Spes (kreso@cateia.com)
 
-class GuiFrameListener : public OIS::KeyListener, public OIS::MouseListener, public ExampleFrameListener
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License (LGPL) as published by the 
+Free Software Foundation; either version 2 of the License, or (at your option) 
+any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+*************************************************************************************/
+#include "TheoraDemoApp.h"
+
+
+
+namespace Ogre
 {
-private:
-    bool mShutdownRequested;
-	bool init;
-	//ClipListener mMovieListener;
 
-public:
-    // NB using buffered input, this is the only change
-	GuiFrameListener(RenderWindow* win, Camera* cam)
-        : ExampleFrameListener(win, cam, true, true, true), 
-          mShutdownRequested(false),
-		  init(false)
-		 // mMovieListener()
-    {
-		mMouse->setEventCallback(this);
-		mKeyboard->setEventCallback(this);
-    }
-
-    void requestShutdown(void)
-    {
-        mShutdownRequested = true;
-    }
-
-	bool frameStarted(const FrameEvent& evt)
-    {
-        if (mShutdownRequested)
-            return false;
-        else
-            return ExampleFrameListener::frameStarted(evt);
-    }
-
-	//----------------------------------------------------------------//
-	bool mouseMoved( const OIS::MouseEvent &arg )
+	class DemoApp : public TheoraDemoApp
 	{
-		return true;
-	}
-
-	//----------------------------------------------------------------//
-	bool mousePressed( const OIS::MouseEvent &arg, OIS::MouseButtonID id )
-	{
-		return true;
-	}
-
-	//----------------------------------------------------------------//
-	bool mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id )
-	{
-
-		return true;
-	}
-
-	//----------------------------------------------------------------//
-	bool keyPressed( const OIS::KeyEvent &arg )
-	{
-		if( arg.key == OIS::KC_ESCAPE )
-			mShutdownRequested = true;
-		return true;
-	}
-
-	//----------------------------------------------------------------//
-	bool keyReleased( const OIS::KeyEvent &arg )
-	{
-		return true;
-	}
-};
-
-
-class GuiApplication : public ExampleApplication
-{
-private:
-	bool mShaders;
-public:
-
-	GuiApplication() :
-		mShaders(false)
-    {
-
-
-
-	}
-
-    ~GuiApplication()
-    {
-		return;
-    }
-
-protected:
-    // Just override the mandatory create scene method
-    void createScene(void)
-    {
-		mCamera->getViewport()->setBackgroundColour(ColourValue(0.3,0.3,0.3));
-
-		float left[]=  {   -1, 0.01,    -1,  0.01};
-		float top[]=   {    1,    1, -0.01, -0.01};
-		float right[]= {-0.01,    1, -0.01,     1};
-		float bottom[]={ 0.01, 0.01,    -1,    -1};
-		String materials[]={"konqi","fedora01","fedora02","fedora03"};
-		for (int i=0;i<4;i++)
+	public:
+		DemoApp()
 		{
-			ManualObject* model = mSceneMgr->createManualObject("quad"+StringConverter::toString(i));
-			model->begin(materials[i]);
+		}
 
-			model->position( right[i],bottom[i],0);
-			model->textureCoord(1,1);
+		void frameStarted(const FrameEvent& evt)
+		{
 
-			model->position( right[i],top[i],0);
-			model->textureCoord(1,0);
-
-			model->position(left[i],top[i],0);
-			model->textureCoord(0,0);
-
-			model->position(left[i],top[i],0);
-			model->textureCoord(0,0);
-
-			model->position( right[i],bottom[i],0);
-			model->textureCoord(1,1);
-
-			model->position(left[i],bottom[i],0);
-			model->textureCoord(0,1);
-
-			model->end();
-
-			model->setUseIdentityProjection(true);
-			model->setUseIdentityView(true);
-			
-
-			SceneNode* node = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-			node->attachObject(model);
 		}
 
 
-    }
+		void init()
+		{
+			createQuad("01","konqi"   ,   -1,    1,-0.01, 0.01);
+			createQuad("02","fedora01", 0.01,    1,    1, 0.01);
+			createQuad("03","fedora02",   -1,-0.01,-0.01,   -1);
+			createQuad("04","fedora03", 0.01,-0.01,    1,   -1);
+		}
+	};
 
-    // Create new frame listener
-    void createFrameListener(void)
-    {
-        mFrameListener= new GuiFrameListener(mWindow, mCamera);
-        mRoot->addFrameListener(mFrameListener);
-    }
-
-	void loadResources()
-	{
-		ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
-    }
-};
-
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-#define WIN32_LEAN_AND_MEAN
-
-
-INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
-#else
-int main(int argc, char *argv[])
-#endif
-{
-
-    // Create application object
-    GuiApplication app;
-
-    try {
-        app.go();
-    } catch( Ogre::Exception& e ) {
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-        MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
-#else
-        std::cerr << "An exception has occured: " <<
-            e.getFullDescription().c_str() << std::endl;
-#endif
-    }
-
-
-    return 0;
+	TheoraDemoApp* start() { return new DemoApp(); }
 }
