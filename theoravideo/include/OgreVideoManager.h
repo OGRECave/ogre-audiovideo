06215d6ed7a634e22bdd8feb353203ca80b2568d
Robert Paciorek <robert@opcode.eu.org>
2022-02-04 10:34:26 +0000

    Ogre 2.3 support in theoravideo
    * support new texture API (Ogre >= 2.2) in theoravideo

--- theoravideo/include/OgreVideoManager.h
+++ theoravideo/include/OgreVideoManager.h
@@ -91,7 +91,11 @@ namespace Ogre
 	private:
 		struct ClipTexture {
 			TheoraVideoClip*  clip;
+			#if OGRE_VERSION_MAJOR == 2 and OGRE_VERSION_MINOR >= 2
+			TextureGpu*       texture;
+			#else
 			TexturePtr        texture;
+			#endif
 		};
 		std::map<String,ClipTexture> mClipsTextures;
 		bool mbInit;
@@ -103,6 +107,10 @@ namespace Ogre
 		// This function is called to init this plugin - do not call directly
 		bool initialise();
 		void shutDown();
+		
+		#if OGRE_VERSION_MAJOR == 2 and OGRE_VERSION_MINOR >= 2
+		void fillTexture(TextureGpu* texture, const uint8* data, int xSize, int ySize);
+		#endif
 	};
 	
 	class _OgreTheoraExport OgreVideoPlugin : public Plugin
